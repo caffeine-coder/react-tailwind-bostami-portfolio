36c839442c5dd0cc1261b7222d47610f311d9b3f
Abhishek P <abhishek_haptech@outlook.com>
2024-08-06 23:32:44 +0530

    Complete contact form (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
       fontFamily: {
         robotoslab: ['Roboto Slab', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
