76ea0a81f669e2da971fd332f128244e0c2709e8
Abhishek P <abhishek_haptech@outlook.com>
2024-08-07 11:17:27 +0530

    Add scroll for sections (#4)

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,53 +1,55 @@
-import React from 'react';
+// src/Components/About/About.js
+import React, { forwardRef } from 'react';
 import { FaCode, FaInfinity } from 'react-icons/fa6';
 import DetailCard from '../DetailCard/DetailCard';
 import Home from '../../assets/images/Home.png';
 
-function About() {
-  return (
-    <div className="flex flex-col bg-white m-5 mt-10 rounded-[18px] items-start p-5">
-      <div className="flex flex-row  m-4 mt-10 items-center ">
-        <h1 className="font-bold text-3xl font-robotoslab">About</h1>
-        <div className="bg-gradient-to-r to-pink-500 from-rose-500  w-[10rem] h-[0.2rem] rounded-lg ml-6" />
-      </div>
-      <div className="flex flex-col items-center 2xl:flex-row 2xl:items-center 2xl:justify-around m-4  ">
-        <img src={Home} alt="Home" className="w-96 lg:w-[30rem]" />
-        <p className="font-poppins text-gray-600 tracking-wider lg:tracking-normal lg:text-lg">
-          Senior developer experienced in developing high end, scalable and
-          responsive web applications, REST Apis, SPAs with technologies
-          including ReactJS with Redux, Ruby on Rails, Java and Mysql.
-          <br />
-          <br />
-          My expertise are React, Javascript, Java and Spring boot. I have also
-          worked on devops including cloud services with AWS and OCI, Jenkins,
-          Docker, Terraform, Prometheus and monitoring apps.
-        </p>
-      </div>
-      <div className="flex flex-col w-full">
-        <h1 className="font-medium text-2xl font-poppins mt-5 ml-4 mb-6">
-          What I Do!
-        </h1>
-        <div className="flex flex-col lg:flex-row lg:justify-start lg:items-start">
-          <div className="flex flex-col">
-            <DetailCard
-              classname="flex flex-col bg-rose-50 w-[95%] self-center p-2 rounded-2xl mb-5 shadow-md lg:m-4"
-              header={<FaCode className="text-red-500" />}
-              title="Web Development"
-              description="Have experience working in various web technologies, languages and frameworks including Java, JavaScript, Spring and Spring Boot"
-            />
-          </div>
-          <div className="flex flex-col">
-            <DetailCard
-              classname="flex flex-col bg-sky-100 w-[95%] self-center p-2 rounded-2xl mb-5 shadow-md lg:m-4"
-              header={<FaInfinity className="text-purple-500" />}
-              title="DevOps & Cloud"
-              description="Have experience working on different cloud technologies including AWS & OCI, along with setting up devops pipeline for each of them"
-            />
-          </div>
+const About = forwardRef((props, ref) => (
+  <div
+    ref={ref}
+    className="flex flex-col bg-white m-5 mt-10 rounded-[18px] items-start p-5"
+  >
+    <div className="flex flex-row m-4 mt-10 items-center">
+      <h1 className="font-bold text-3xl font-robotoslab">About</h1>
+      <div className="bg-gradient-to-r to-pink-500 from-rose-500 w-[10rem] h-[0.2rem] rounded-lg ml-6" />
+    </div>
+    <div className="flex flex-col items-center 2xl:flex-row 2xl:items-center 2xl:justify-around m-4">
+      <img src={Home} alt="Home" className="w-96 lg:w-[30rem]" />
+      <p className="font-poppins text-gray-600 tracking-wider lg:tracking-normal lg:text-lg">
+        Senior developer experienced in developing high end, scalable and
+        responsive web applications, REST Apis, SPAs with technologies including
+        ReactJS with Redux, Ruby on Rails, Java and Mysql.
+        <br />
+        <br />
+        My expertise are React, Javascript, Java and Spring boot. I have also
+        worked on devops including cloud services with AWS and OCI, Jenkins,
+        Docker, Terraform, Prometheus and monitoring apps.
+      </p>
+    </div>
+    <div className="flex flex-col w-full">
+      <h1 className="font-medium text-2xl font-poppins mt-5 ml-4 mb-6">
+        What I Do!
+      </h1>
+      <div className="flex flex-col lg:flex-row lg:justify-start lg:items-start">
+        <div className="flex flex-col">
+          <DetailCard
+            classname="flex flex-col bg-rose-50 w-[95%] self-center p-2 rounded-2xl mb-5 shadow-md lg:m-4"
+            header={<FaCode className="text-red-500" />}
+            title="Web Development"
+            description="Have experience working in various web technologies, languages and frameworks including Java, JavaScript, Spring and Spring Boot"
+          />
+        </div>
+        <div className="flex flex-col">
+          <DetailCard
+            classname="flex flex-col bg-sky-100 w-[95%] self-center p-2 rounded-2xl mb-5 shadow-md lg:m-4"
+            header={<FaInfinity className="text-purple-500" />}
+            title="DevOps & Cloud"
+            description="Have experience working on different cloud technologies including AWS & OCI, along with setting up devops pipeline for each of them"
+          />
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+));
 
 export default About;
--- src/Components/ProfileContainer/ProfileContainer.jsx
+++ src/Components/ProfileContainer/ProfileContainer.jsx
@@ -1,28 +1,37 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
-import { FaInstagram, FaLinkedinIn, FaMobileAlt } from 'react-icons/fa';
+import {
+  FaInstagram,
+  FaLinkedinIn,
+  FaMobileAlt,
+  FaYoutube,
+} from 'react-icons/fa';
 import { MdOutlineMail } from 'react-icons/md';
 import { IoLocationSharp } from 'react-icons/io5';
 import { LuCalendarDays } from 'react-icons/lu';
 import { PiDownloadLight } from 'react-icons/pi';
 import abhi from '../../assets/images/abhi.jpg';
+import oracle from '../../assets/images/oracle.png';
 import SocialCard from '../SocialCard/SocialCard';
 import PersonalDetailCard from '../PersonalDetailCard/PersonalDetailCard';
 
 function ProfileContainer() {
   return (
-    <div className="flex flex-col bg-white m-5 mt-10 rounded-[18px] justify-center items-center p-5 lg:justify-start">
-      <div className="mt-11 mb-7 w-[14rem] h-[14rem] rounded-lg md:w-[16rem] md:h-[16rem] lg:w-[10rem] lg:h-[10rem]">
+    <div className="flex flex-col bg-white m-5 mt-10 lg:mt-40 rounded-[18px] justify-center items-center p-5 lg:justify-start">
+      <div className=" mt-11 lg:mt-0 lg:absolute lg:top-20 mb-7 w-[14rem] h-[14rem] rounded-lg md:w-[16rem] md:h-[16rem] lg:w-[10rem] lg:h-[10rem] ">
         <img src={abhi} className="rounded-[18px] object-fill" alt="Profie" />
       </div>
-      <div className=" text-2xl md:text-3xl lg:text-xl">
+      <div className=" text-2xl md:text-3xl lg:text-xl lg:mt-20">
         <p className="font-robotoslab">Abhishek P</p>
       </div>
       <div className="bg-slate-100 rounded-lg mt-3 mb-4 p-1">
         <p className="font-poppins font-[550] m-2 md:text-lg lg:text-sm">
           {' '}
           Full Stack Developer
+          <div className="flex items-center justify-center">
+            <img src={oracle} alt="Oracle" className="w-[80px] h-[20px]" />
+          </div>
         </p>
       </div>
       <div className="flex flex-row w-[12rem] h-[2rem] mb-5 justify-around ">
@@ -35,6 +44,16 @@ function ProfileContainer() {
             icon={
               <FaInstagram className="text-red-500 md:text-xl lg:text-sm" />
             }
+            className="bg-gray-100 rounded-lg justify-center items-center w-[2.3rem] h-[2.3rem] flex md:w-[2.6rem] md:h-[2.6rem] lg:w-[2.2rem] lg:h-[2.2rem]"
+          />
+        </div>
+        <div
+          onClick={() => {
+            window.open('https://www.youtube.com/caffeine-coders', '_blank');
+          }}
+        >
+          <SocialCard
+            icon={<FaYoutube className="text-red-500 md:text-xl lg:text-sm" />}
             className="bg-gray-100 rounded-lg justify-center items-center w-[2.3rem] h-[2.3rem] flex md:w-[2.6rem] md:h-[2.6rem] lg:w-[2.2rem] lg:h-[2.2rem]"
           />
         </div>
